14163b1d6d1624949d0a768f9f82bd35b8856d2e
Nikita Menshikov <menshikov.mikita@gmail.com>
2021-12-12 14:35:06 +0300

    First commit

---
+++ src/main/java/com/bsuir/mercury/CasinoKursachApplication.java
@@ -0,0 +1,13 @@
+package com.bsuir.mercury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoKursachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoKursachApplication.class, args);
+	}
+
+}
